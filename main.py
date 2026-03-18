98421cd03e502d7162726dff210c8d60c0acad52
dadnator <massonhalima29@gmail.com>
2026-03-18 08:09:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,8 @@ async def setup_ping_button(interaction: discord.Interaction):
 @app_commands.default_permissions(administrator=True) 
 async def perco_immediate(interaction: discord.Interaction):
     # 1. On récupère les infos nécessaires
-    role_id = ROLES_PING["Sleeping"]["id"]
+    role_id = ROLES_PING["DÉFENSE PERCO"]["id"]
+    await start_spam_logic(interaction, role_id)
     perco_channel = interaction.client.get_channel(PERCO_CHANNEL_ID)
     role_mention = f"<@&{role_id}>"
     
